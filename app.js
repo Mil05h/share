a64c0157593bb79910bff0bec36118767bb0c6fd
Milos <lekolekovic@gmail.com>
2022-12-18 21:33:30 +0100

    adding password to aws_rds

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ const options = {
     host: process.env.AWS_RDS_HOST,
     port: process.env.AWS_RDS_PORT,
     user: process.env.AWS_RDS_USER,
-    // password: process.env.AWS_RDS_PASSWORD,
+    password: process.env.AWS_RDS_PASSWORD,
     // database: process.env.AWS_RDS_DATABASE,
 };
 
