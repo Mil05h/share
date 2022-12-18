6b48a967c153bbd237e9837aace87fbe1761e80a
Mil05h <104977149+Mil05h@users.noreply.github.com>
2022-12-18 23:24:55 +0100

    added password and db name to connection

--- middleware.js
+++ middleware.js
@@ -49,8 +49,8 @@ module.exports.userExists = (req, res, next) => {
         host: process.env.AWS_RDS_HOST,
         port: process.env.AWS_RDS_PORT,
         user: process.env.AWS_RDS_USER,
-        // password: process.env.AWS_RDS_PASSWORD,
-        // database: process.env.AWS_RDS_DATABASE,
+        password: process.env.AWS_RDS_PASSWORD,
+        database: process.env.AWS_RDS_DATABASE,
         multipleStatements: true
     });
     ////////////////////////////// DEVELOPMENT LOCAL DB /////////////////////////////
